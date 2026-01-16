4e49bcc2562d23806938f9895ec31dc2c6acb1f6
nak8saio <nak8sio@gmail.com>
2026-01-16 14:56:39 +0000

    Day 1: Python variables and profile program

---
+++ day1_variable.py
@@ -0,0 +1,16 @@
+# Profile management
+name = "Alexa"
+age = 30
+address = "India"
+monthly_salary = 80000
+annual_salary = monthly_salary*12
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Location: {address}")
+print(f"Your annual salary is: {annual_salary}")
+
+if annual_salary >= 100000:
+    print(f"Your salary is high income group.")
+else:
+    print(f"You have to learn more to get better salary!")
